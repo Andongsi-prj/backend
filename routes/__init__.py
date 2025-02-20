f628481f460962b3a6524bc4eafb2f92ec4b7914
gh159159 <gh159159@gmail.com>
2025-02-20 17:34:48 +0900

    25.02.20

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,4 @@
 from .site_route import site_route
-from .pipe_route import pipe_route
 from .login_route import login_route
 from .loginck_route import loginck_route
 from .images_route import images_route
@@ -10,7 +9,6 @@ from .slack_route import slack_route
 
 blueprints = [
    (site_route,"/"),
-   (pipe_route,"/api/pipe"),
    (login_route,"/api/login"),
    (loginck_route,"/api/loginck"),
    (images_route,"/api/images"),
